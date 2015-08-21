807f4001146f968ca9ef0bac190f6a7ad412d2ec
hungerr <xuefeihan@126.com>
2015-08-21 11:47:34 +0800

    111

--- messagepush/admin.py
+++ messagepush/admin.py
@@ -34,6 +34,8 @@ class HelpAdmin(admin.ModelAdmin):
                 ret=iosx.PushTags(0, idlist, 'AND', iosmsg, 1)
             if len(idlist)==1:
                 ret=x.PushTags(0, idlist, 'OR', msg)
+                obj.title=ret[0]
+                obj.save()
                 ret=iosx.PushTags(0, idlist, 'OR', iosmsg, 1)
 class MSGAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
