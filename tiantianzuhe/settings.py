3c72bfc517fe4380050ba6407c55fface19c80eb
hungerr <xuefeihan@126.com>
2015-08-25 14:57:50 +0800

    111

--- tiantianzuhe/settings.py
+++ tiantianzuhe/settings.py
@@ -63,15 +63,15 @@ WSGI_APPLICATION = 'tiantianzuhe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        #'ENGINE': 'django.db.backends.mysql',
-        #'NAME': 'tiantian',
-        #'USER': 'root',
-        #'PASSWORD': 'root',
-        #'HOST': '',
-        #'PORT': '',
-        #'OPTIONS': {'charset':'utf8mb4'},
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tiantian',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {'charset':'utf8mb4'},
     }
 }
 
