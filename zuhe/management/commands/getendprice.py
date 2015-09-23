bd3a0fea593196453843ce0c2c7f56f15ed25755
hungerr <xuefeihan@126.com>
2015-09-23 18:06:24 +0800

    111

--- zuhe/management/commands/getendprice.py
+++ zuhe/management/commands/getendprice.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from zuhe.models import Zuhe
+from zuhe.models import Zuhe,StockPrice
 import datetime
 import requests
 import json
@@ -53,3 +53,22 @@ class Command(BaseCommand):
                     zuhe.toprate=zuhe.rate
                 zuhe.updatedate=datetime.datetime.now() 
                 zuhe.save()
+        zuhes_2=Zuhe.objects.filter(starttime__lte=now,endtime__gte=today)
+        for zuhe in zuhes_2:
+            startdate=zuhe.starttime.strftime('%Y-%m-%d')
+            enddate=zuhe.endtime.strftime('%Y-%m-%d')
+            for stock in zuhe.singlestock_set.all():     
+                code=stock.code
+                url='http://mkt.bankuang.com/kline.php?symbol=%s&q_type=2&fq=1&stime=%s&etime=%s&r_type=2'%(code,startdate,enddate)
+                r=requests.get(url)
+                try:
+                    datas=json.loads(r.content)
+                    for data in datas:
+                        price=str(data.get('Close',''))
+                        date=data.get('Date','')
+                        date=datetime.date(year=int(date[:4]),month=int(date[5:7]),day=int(date[8:10]))
+                        if not StockPrice.objects.filter(stock=stock,date=date).exists():
+                            StockPrice.objects.create(stock=stock,date=date,price=price)
+                      
+                except:
+                    pass
